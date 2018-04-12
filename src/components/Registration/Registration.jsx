722af3222e328f288618f0b77861848445db3e18
Marcel Engler <mengler@united-internet.de>
2018-04-12 12:28:48 +0200

    added registration Component structure

---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Registration = props => (
+    <div>
+        Registration    
+    </div>
+)
+
+export default Registration;
--- src/container/App/App.jsx
+++ src/container/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Login from '../../components/Login/Login.jsx';
+import Registration from '../../components/Registration/Registration.jsx';
 import UIWrapper from '../../components/UIWrapper/UIWrapper.jsx';
 import { Route } from 'react-router-dom';
 
@@ -15,11 +16,16 @@ class App extends Component {
                     {
                         value: 'Login',
                         path: '/login'
+                    },
+                    {
+                        value: 'Registrieren',
+                        path: '/registration'
                     }
                 ]}
 
             >
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/registration" component={Registration} />
             </UIWrapper>
         );
     }
