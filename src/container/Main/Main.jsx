d55770d8a10ce4bfd7ed21d40e04d42d1ca7fe10
Marcel Engler <mengler@united-internet.de>
2018-05-14 09:07:35 +0200

    dumb rework - Main

--- src/container/Main/Main.jsx
+++ src/container/Main/Main.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import MainDumb from '../../components/Main/Main';
+import { Row, Col, Button } from 'react-bootstrap';
+import Calendar from 'react-calendar';
 
+import style from './style_module.css';
 
 import { actions } from '../../actions';
+import { popupId } from '../../constants';
 
 class Main extends Component {
     constructor(props) {
@@ -17,10 +20,42 @@ class Main extends Component {
     }
     render() {
         return (
-            <MainDumb
-                setPopupId={this.props.setPopupId}
-                ownCalendars={this.formatOwnCalendar()}
-            />
+            <Row className={style.rowHeightCorrection}>
+                <Col className={style.sideBarWrapper} xs={12} md={3} lg={2}>
+                    <Calendar className={style.calendar} />
+
+                    <div className={style.textCentering}>
+                        <h3>Kalender</h3>
+                        {
+                            this.formatOwnCalendar().map((calendar, index) =>
+                                <Button
+                                    key={'ownCalendarFilter-' + index}
+                                    className={style.large + ' ' + style.marginBottom}
+                                    bsStyle="success"
+                                // onClick={() => { props.setPopupId(popupId.MANAGE_CALENDAR) }}
+                                >
+                                    {calendar.calendar_title}
+                                </Button>
+                            )
+                        }
+                        <Button
+                            className={style.large}
+                            bsStyle="primary"
+                            onClick={() => { this.props.setPopupId(popupId.MANAGE_CALENDAR) }}
+                        >
+                            Kalender verwalten
+                        </Button>
+                        <hr />
+                        <Button
+                            className={style.large}
+                            bsStyle="primary"
+                            onClick={() => { this.props.setPopupId(popupId.ADD_CALENDAR) }}
+                        >
+                            Kalender hinzufügen
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
         );
     }
 }
---
+++ src/container/Main/reducerUi.js
@@ -0,0 +1,15 @@
+import {
+    actionNames
+} from '../../actions';
+
+import {
+    getCookie
+} from '../../globalFunctions';
+
+export default (state = {
+}, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
