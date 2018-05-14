701f4915b01019b3442702e0ec098dbb6f90c1fb
Marcel Engler <mengler@united-internet.de>
2018-05-14 08:07:35 +0200

    dumb rework - Login

--- src/container/Login/Login.jsx
+++ src/container/Login/Login.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import LoginDumb from '../../components/Login/Login.jsx';
+import { FormGroup, FormControl, Checkbox, Collapse, Alert, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 import { actions } from '../../actions';
 import { proxyToValue, proxyToName } from '../../globalFunctions';
-
-
+import Centering from '../../components/Centering/Centering';
+import style from './style_module.css';
+import Card from '../../components/Card/Card';
+import LoadingButton from '../../components/LoadingButton/LoadingButton';
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -29,14 +33,65 @@ class Login extends Component {
     }
     render() {
         return (
-            <LoginDumb
-                {...this.props.ui}
-                {...this.props.data}
+            <Centering x y className={style.width}>
+                <Card>
+                    <FormGroup>
+                        <h3><b>Login</b></h3>
+                    </FormGroup>
+                    <FormGroup validationState={this.props.data.error ? 'error' : null}>
+                        <FormControl
+                            type="text"
+                            value={this.props.ui.username}
+                            name="username"
+                            placeholder="Nutzername"
+                            onChange={this.handleUserinput}
+                            disabled={this.props.data.loading}
+                        />
 
-                handleStayLoggedInCheckbox={this.handleStayLoggedInCheckbox}
-                handleSubmit={this.handleSubmit}
-                handleUserinput={this.handleUserinput}
-            />
+                    </FormGroup>
+                    <FormGroup validationState={this.props.data.error ? 'error' : null}>
+                        <FormControl
+                            type="password"
+                            value={this.props.ui.password}
+                            name="password"
+                            placeholder="Passwort"
+                            onChange={this.handleUserinput}
+                            disabled={this.props.data.loading}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Checkbox checked={this.props.ui.stayLoggedIn} onClick={this.handleStayLoggedInCheckbox}>
+                            eingeloggt bleiben
+                        </Checkbox>
+                    </FormGroup>
+                    <Collapse in={this.props.data.error}>
+                        <FormGroup>
+                            <Alert bsStyle="danger">
+                                Passwort oder Nutzername falsch.
+                            </Alert>
+                        </FormGroup>
+                    </Collapse>
+                    <FormGroup>
+                        <LoadingButton loading={this.props.data.loading}>
+                            <div>
+                                <Button
+                                    className={style.large}
+                                    bsStyle="success"
+                                    onClick={this.handleSubmit}
+                                    disabled={this.props.ui.username === '' || this.props.ui.password === ''}
+                                >
+                                    Login
+                            </Button>
+                            </div>
+                        </LoadingButton>
+                    </FormGroup>
+                    <FormGroup>
+                        <Link to='/registration'>
+                            Registrieren
+                        </Link>
+                    </FormGroup>
+                </Card>
+            </Centering >
         );
     }
 }
