fcf41fc2399e9b46a238006335472e919ba3ef64
Marcel Engler <mengler@united-internet.de>
2018-06-11 09:04:36 +0200

    added parser in calendar search

--- src/apiConnector.js
+++ src/apiConnector.js
@@ -76,7 +76,7 @@ export default {
                     )
                 )
                 .then((...responses) => {
-                    resolve(responses)
+                    resolve(parser(responses))
                 })
                 .catch(e => {
                     reject(e);
