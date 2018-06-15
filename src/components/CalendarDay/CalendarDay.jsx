62ab9a786433227e89d66ba07e9eb2f206bdf024
Marcel Engler <mengler@united-internet.de>
2018-06-15 12:16:52 +0200

    added ordering of overlapping appointments

--- src/components/CalendarDay/CalendarDay.jsx
+++ src/components/CalendarDay/CalendarDay.jsx
@@ -104,62 +104,42 @@ const getHeight = (start, end) => {
     return (end - start) / millisecondsOfDay * 100 + "%"
 }
 
-const getWidth = appointment => {
-    const sl = appointment.startConflicts.length;
-    const el = appointment.endConflicts.length;
-    const addition = sl === 0 || el === 0 ? 1 : 0
-    return 100 / (sl + el + addition)
-}
-
-const getMarginLeft = appointment => {
-    let diff = appointment.startConflicts.length - appointment.endConflicts.length;
-    if (diff < 0) {
-        diff *= -1;
-    }
-    if (diff === 0) {
-        diff = 1;
-    }
-    return 100 / diff
-}
-
-const orderByStart = appointments => appointments.sort((a1, a2) => a1.start > a2.start)
-
+const orderBySize = appointments => appointments.sort(
+    (a1, a2) =>
+        a1.end - a1.start < a2.end - a2.start
+)
 
 const adjustAppointments = appointments => {
-    let checkedAppointmentSteps = [[]];
-    appointments.forEach(appointment => {
-        const stepIndex = checkedAppointmentSteps.length - 1;
-        const insideStepIndex = checkedAppointmentSteps[stepIndex].length;
-        const lastAppointmentInStep = [insideStepIndex];
-        console.log(checkedAppointmentSteps[stepIndex].length)
-        if (checkedAppointmentSteps[stepIndex].length === 0) {
-            if (
-                checkedAppointmentSteps[stepIndex].find(
-                    appointemtInStep => moment(appointment.start+1).isBefore(appointemtInStep.end)
-                )
-            ) {
-                checkedAppointmentSteps[stepIndex].push(appointment)
-            } else {
-                checkedAppointmentSteps.push([appointment])
-            }
-        } else {
-            checkedAppointmentSteps[stepIndex].push(appointment)
+    let alreadyMappedAppointments = [];
+    return orderBySize(appointments).map(data => {
+
+        const leftIndex = alreadyMappedAppointments.filter(appointment =>
+            moment(data.start).isBetween(appointment.start - 1, appointment.end + 1) ||
+            moment(data.end).isBetween(appointment.start - 1, appointment.end + 1)
+        ).length;
+
+        alreadyMappedAppointments.push(data);
+
+        return {
+            data,
+            leftIndex
         }
-        console.log(checkedAppointmentSteps[0], checkedAppointmentSteps[1])
+    }).map(appointment => {
+        const widthIndexCount = alreadyMappedAppointments.filter(alreadyMappedAppointment =>
+            moment(appointment.data.start).isBetween(alreadyMappedAppointment.start - 1, alreadyMappedAppointment.end + 1) ||
+            moment(appointment.data.end).isBetween(alreadyMappedAppointment.start - 1, alreadyMappedAppointment.end + 1)
+        ).length;
+        return {
+            data: appointment.data,
+            style: {
+                top: getTopMargin(appointment.data.start),
+                height: getHeight(appointment.data.start, appointment.data.end),
+                left: appointment.leftIndex * 100 / widthIndexCount + "%",
+                width: 100 / widthIndexCount + "%",
+                backgroundColor: getColorOfLetter(appointment.data.appointment_title[0] + "", appointment.data.appointment_title[1] + "")
+            }
+        }
     })
-    console.log()
-    return [];
-    //     return {
-    //         data: appointment.data,
-    //         style: {
-    //             top: getTopMargin(appointment.data.start),
-    //             height: getHeight(appointment.data.start, appointment.data.end),
-    //             left: marginLeft + "%",
-    //             width: width + "%",
-    //             backgroundColor: getColorOfLetter(appointment.data.appointment_title[0] + "", appointment.data.appointment_title[1] + "")
-    //         }
-    //     }
-    // })
 }
 
 
