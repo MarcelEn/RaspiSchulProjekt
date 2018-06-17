b73de038ab8f449b7271606f680d516404ebc018
Marcel Engler <engma98@gmx.de>
2018-06-17 15:56:20 +0200

    fetch appointments on calendar filter toggle

--- src/container/Main/middleware.js
+++ src/container/Main/middleware.js
@@ -10,13 +10,23 @@ import {
     setCalendarFilter
 } from '../../globalFunctions';
 
+import API from './../../apiConnector';
+
 export function* toggleMainCalendarFilter(action) {
     let currentCalendarFilter = getCalendarFilter();
+    
+    let fetch = false;
 
     if (currentCalendarFilter.find(calendarId => calendarId === action.payload)) {
         currentCalendarFilter = currentCalendarFilter.filter(calendarId => calendarId !== action.payload);
     } else {
         currentCalendarFilter = [...currentCalendarFilter, action.payload];
+        fetch = true;
+    }
+
+    if(fetch){
+        const response = yield API.searchAppointmentsByCalendarId(action.payload, 0, new Date().valueOf() * 2)
+        yield put(actions.addAppointmentData(response.data))
     }
 
     setCalendarFilter(currentCalendarFilter);
