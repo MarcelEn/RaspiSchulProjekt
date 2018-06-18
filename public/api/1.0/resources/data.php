7bfcd8b125a9a60b4bfdcc7c4c20700105b28ea0
Linus Posselt <linusposselt@gmx.de>
2018-06-18 08:36:29 +0200

    refactoring of data.php

--- public/api/1.0/resources/data.php
+++ public/api/1.0/resources/data.php
@@ -1,36 +1,47 @@
 <?php
+require_once 'class/token_class.php';
 
-$app->post('/data/profile_picture', function ($request, $response, $args) {
+$app->post('/data/profile_picture', function (
+    $request,
+    $response,
+    $args
+) {
     if(!Token::validate())  {
         return $response->withStatus(UNAUTHORIZED);
     }
+
     $files = $request->getUploadedFiles();
     $target_dir = "/var/www/html/profile_picture/";
     $target_file = $target_dir . Token::getUID();
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
     $check = getimagesize($_FILES["profile_picture"]["tmp_name"]);
     if($check == false) {
         $errorString = "File is not a image\n";
         $response->getBody()->write($errorString);
         return $response->withStatus(400);
     }
+
     if ($_FILES["profile_picture"]["size"] > 500000) {
         $errorString = "file is larger than 500KB\n";
         $response->getBody()->write($errorString);
         return $response->withStatus(400);
     }
+
     if (move_uploaded_file($_FILES["profile_picture"]["tmp_name"], $target_file)) {
         return $response->withStatus(201);
-    } else {
-        return $response->withStatus(500);
     }
+    return $response->withStatus(500);
 });
 
-$app->delete('/data/profile_picture', function ($request, $response, $args) {
+$app->delete('/data/profile_picture', function (
+    $request,
+    $response,
+    $args
+) {
     if(!Token::validate())  {
         return $response->withStatus(UNAUTHORIZED);
     }
+
     $target_dir = "/var/www/html/profile_picture/";
     $target_file = $target_dir . Token::getUID();
 
@@ -46,5 +57,4 @@ $app->delete('/data/profile_picture', function ($request, $response, $args) {
     return $respons->withStatus(500);
 
 });
-
 ?>
